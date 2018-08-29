8fbd3625228db07a995e0d6f20cf3e723133fc93
Loksie <41243989+Loksie@users.noreply.github.com>
2018-08-29 23:17:10 +0200

    Initial commit

---
+++ VM/Descriptors/VMDescriptor.cs
@@ -0,0 +1,51 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace KoiVM.VM
+{
+    public class VMDescriptor
+    {
+        public VMDescriptor(IVMSettings settings)
+        {
+            Random = new Random(settings.Seed);
+            Settings = settings;
+            Architecture = new ArchDescriptor(Random);
+            Runtime = new RuntimeDescriptor(Random);
+            Data = new DataDescriptor(Random);
+        }
+
+        public Random Random
+        {
+            get;
+        }
+
+        public IVMSettings Settings
+        {
+            get;
+        }
+
+        public ArchDescriptor Architecture
+        {
+            get;
+        }
+
+        public RuntimeDescriptor Runtime
+        {
+            get;
+        }
+
+        public DataDescriptor Data
+        {
+            get;
+            private set;
+        }
+
+        public void ResetData()
+        {
+            Data = new DataDescriptor(Random);
+        }
+    }
+}
---
+++ VM/VMMethodInfo.cs
@@ -0,0 +1,31 @@
+﻿#region
+
+using System.Collections.Generic;
+using KoiVM.CFG;
+
+#endregion
+
+namespace KoiVM.VM
+{
+    public class VMMethodInfo
+    {
+        public readonly Dictionary<IBasicBlock, VMBlockKey> BlockKeys;
+        public readonly HashSet<VMRegisters> UsedRegister;
+        public byte EntryKey;
+        public byte ExitKey;
+
+        public ScopeBlock RootScope;
+
+        public VMMethodInfo()
+        {
+            BlockKeys = new Dictionary<IBasicBlock, VMBlockKey>();
+            UsedRegister = new HashSet<VMRegisters>();
+        }
+    }
+
+    public struct VMBlockKey
+    {
+        public byte EntryKey;
+        public byte ExitKey;
+    }
+}
